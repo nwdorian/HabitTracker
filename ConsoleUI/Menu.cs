6cfdcc9832339a99b5bdd953d53b10b24da2ba6f
nwdorian <neuwirthdorian@gmail.com>
2024-01-31 22:16:26 +0100

    Add project files.

---
+++ ConsoleUI/Menu.cs
@@ -0,0 +1,58 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConsoleUI.Models.Enums;
+
+namespace ConsoleUI
+{
+    internal class Menu
+    {
+        internal static void MainMenu()
+        {
+            bool repeatMenu = true;
+            while (repeatMenu == true)
+            {
+
+                repeatMenu = false;
+                Console.Clear();
+
+                var selection = AnsiConsole.Prompt(
+                    new SelectionPrompt<MenuSelection>()
+                        .Title("Welcome to [green]Habit tracker[/]\nWhat would you like to do?")
+                        .PageSize(10)
+                        .MoreChoicesText("")
+                        .AddChoices(MenuSelection.ViewAllRecords,
+                                    MenuSelection.InsertRecord,
+                                    MenuSelection.DeleteRecord,
+                                    MenuSelection.UpdateRecord,
+                                    MenuSelection.CloseApplication)
+                                    );
+
+                switch (selection)
+                {
+                    case MenuSelection.ViewAllRecords:
+                        break;
+                    case MenuSelection.InsertRecord:
+                        break;
+                    case MenuSelection.DeleteRecord:
+                        break;
+                    case MenuSelection.UpdateRecord:
+                        break;
+                    case MenuSelection.CloseApplication:
+                        if (!AnsiConsole.Confirm("Are you sure you want to exit?"))
+                        {
+                            repeatMenu = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nGoodbye!");
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Models/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Models
+{
+    internal class Enums
+    {
+        public enum MenuSelection
+        {
+            ViewAllRecords,
+            InsertRecord,
+            DeleteRecord,
+            UpdateRecord,
+            CloseApplication
+        }
+    }
+}
